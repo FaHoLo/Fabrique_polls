dbee7e92573cc166573da093a49f5672dbf10bde
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2020-11-03 19:50:23 +0300

    Add active polls view

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Poll, Answer, Choice
+from .serializers import PollSerializer
 
 
 def index(request):
     return HttpResponse('index page is here')
+
+
+@api_view(['GET'])
+def get_active_polls(request):
+    polls = Poll.objects.filter(active=True)
+    serializer = PollSerializer(polls, many=True)
+    return Response(serializer.data)
+
