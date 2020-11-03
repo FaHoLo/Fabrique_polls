e9219980fbc9219e2df104cd4871533214fef2f3
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2020-11-03 20:54:09 +0300

    Fix url with api root

--- fabrique_pools/urls.py
+++ fabrique_pools/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('api/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
