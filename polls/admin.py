3e4f2fcb605a49f71e0751563c1db70ccd7e7fbb
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2020-11-03 18:25:34 +0300

    Add polls into admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Poll, Question
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 0
+
+
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    search_fields = [
+        'name',
+    ]
+    list_display = [
+        'name',
+        'start_date',
+        'end_date',
+    ]
+
+    inlines = [
+        QuestionInline
+    ]
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ['start_date']
+        return self.readonly_fields
