ac06763848cd58cce76b270e0f55ca2a101bfb23
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2020-11-03 20:41:48 +0300

    Add user voted polls view

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .models import Poll, Answer, Choice
+from .models import Poll, Answer
 from .serializers import PollSerializer
 
 
@@ -14,5 +14,27 @@ def index(request):
 def get_active_polls(request):
     polls = Poll.objects.filter(active=True)
     serializer = PollSerializer(polls, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_user_voted_polls(request, user_id):
+    # TODO optimize requests to DB
+    polls = Poll.objects.prefetch_related('questions')
+    answers = Answer.objects.filter(user_id=user_id).select_related('question')
+
+    user_questions = set()
+    for answer in answers:
+        user_questions.add(answer.question)
+
+    user_polls = set()
+    for poll in polls:
+        poll_questions = poll.questions.all()
+        for question in poll_questions:
+            if question in user_questions:
+                user_polls.add(poll)
+                break
+
+    serializer = PollSerializer(user_polls, many=True)
     return Response(serializer.data)
 
