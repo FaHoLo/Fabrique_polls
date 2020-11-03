9f50ce2c8e25c1f66fc073d23e7178503f6e7e3d
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2020-11-03 19:17:01 +0300

    Add choice and answer models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils import timezone
 
@@ -37,8 +38,30 @@ class Question(models.Model):
     class Meta:
         verbose_name = 'Вопрос'
         verbose_name_plural = 'Вопросы'
-    
 
 
 class Choice(models.Model):
-    pass
+    question = models.ForeignKey(Question, verbose_name='варианты ответа',
+                                 on_delete=models.CASCADE, related_name='choices')
+    text = models.CharField('ответ', max_length=200)
+
+    class Meta:
+        verbose_name = 'Вариант ответа'
+        verbose_name_plural = 'Варианты ответа'
+
+    def __str__(self):
+        return f'{self.id} {self.text}'
+
+
+class Answer(models.Model):
+    user_id = models.IntegerField('id пользователя', validators=[MinValueValidator(1)])
+    question = models.ForeignKey(Question, verbose_name='вопрос', on_delete=models.CASCADE,
+                                 related_name='answers')
+    choice = models.ManyToManyField(Choice, related_name='answers')
+
+    class Meta:
+        verbose_name = 'Ответ пользователя на вопрос'
+        verbose_name_plural = 'Ответы пользователя на вопрос'
+
+    def __str__(self):
+        return f'{self.user_id} {self.question} {self.choice}'
