f5019917f36d6f9a8b7d766dc530f1c0b471424b
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2020-11-04 17:16:55 +0300

    Fix user endpoints

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('polls/', views.get_active_polls, name='active_polls'),
-    path('user/<int:user_id>', views.get_user_voted_polls, name='user_polls'),
+    path('polls/active/', views.get_active_polls, name='active_polls'),
+    path('polls/user/<int:user_id>', views.get_user_voted_polls, name='user_polls'),
     path('vote/', views.get_user_vote, name='user_vote')
 ]
